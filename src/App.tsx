a85ecb8294b306b2ec0af444d4cda9e35ab1728b
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-09-23 22:36:27 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Advella web-app.
+          Advella WebApp - Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );

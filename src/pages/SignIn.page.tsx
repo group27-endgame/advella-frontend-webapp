3b020c456c382d52a80e8b9980427402b20b39c1
Patrik Horný <patulda111@gmail.com>
2022-11-02 11:20:13 +0100

    Update SignIn.page.tsx

--- src/pages/SignIn.page.tsx
+++ src/pages/SignIn.page.tsx
@@ -7,7 +7,7 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import UserService from "../services/User.service";
